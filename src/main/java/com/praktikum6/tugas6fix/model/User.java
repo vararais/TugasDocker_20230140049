afd9844348ec2a3d9d2235e0b36a56cab0cb1522
vararais <varadigmaisnann@gmail.com>
2026-04-14 14:10:05 +0700

    Menambah kode user java

--- src/main/java/com/praktikum6/tugas6fix/model/User.java
+++ src/main/java/com/praktikum6/tugas6fix/model/User.java
@@ -1,4 +1,14 @@
 package com.praktikum6.tugas6fix.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private String nama;
+    private String nim;
+    private String jenisKelamin;
 }
