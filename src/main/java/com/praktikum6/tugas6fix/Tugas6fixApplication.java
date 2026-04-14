b666ca957eace360747cca07fab5c1f6a2fb9d17
vararais <varadigmaisnann@gmail.com>
2026-04-14 13:44:18 +0700

    Initial commit

---
+++ src/main/java/com/praktikum6/tugas6fix/Tugas6fixApplication.java
@@ -0,0 +1,13 @@
+package com.praktikum6.tugas6fix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas6fixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tugas6fixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praktikum6/tugas6fix/Tugas6fixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praktikum6.tugas6fix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tugas6fixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
